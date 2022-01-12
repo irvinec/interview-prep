4c8e1eee09e603c9996bdf6987e6deea55fd8870
Casey Irvine <44978150+cirvine-MSFT@users.noreply.github.com>
2022-01-12 12:05:56 -0800

    Adding graph search algos in csharp

---
+++ graph-search-csharp/GraphSearch/Graph.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphSearch
+{
+    public class Graph<T>
+    {
+        public Graph(bool isDirected = true)
+        {
+            IsDirected = isDirected;
+        }
+
+        public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges, bool isDirected = true) : this(isDirected)
+        {
+            foreach (var vertex in vertices)
+            {
+                AddVertex(vertex);
+            }
+
+            foreach (var edge in edges)
+            {
+                AddEdge(edge);
+            }
+        }
+
+        public Dictionary<T, HashSet<T>> AdjacencyList => new Dictionary<T, HashSet<T>>();
+        public bool IsDirected { get; }
+
+        public void AddVertex(T vertex)
+        {
+            AdjacencyList[vertex] = new HashSet<T>();
+        }
+
+        public void AddEdge(Tuple<T, T> edge)
+        {
+            if (!AdjacencyList.ContainsKey(edge.Item1))
+            {
+                AddVertex(edge.Item1);
+            }
+
+            AdjacencyList[edge.Item1].Add(edge.Item2);
+
+            if (!IsDirected)
+            {
+                AdjacencyList[edge.Item2].Add(edge.Item1);
+            }
+        }
+    }
+}
---
+++ graph-search-csharp/GraphSearch/GraphSearch.cs
@@ -0,0 +1,66 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace GraphSearch
+{
+    public static class GraphSearch
+    {
+        public static HashSet<T> DepthFirstSearch<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+            if (!graph.AdjacencyList.ContainsKey(start)) return visited;
+
+            var stack = new Stack<T>();
+            stack.Push(start);
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+                foreach (var adjacent in graph.AdjacencyList[vertex])
+                {
+                    if (!visited.Contains(adjacent))
+                    {
+                        stack.Push(adjacent);
+                    }
+                }
+            }
+
+            return visited;
+        }
+
+        public static HashSet<T> BreadthFirstSearch<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+            if (!graph.AdjacencyList.ContainsKey(start)) return visited;
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+                foreach (var adjacent in graph.AdjacencyList[vertex])
+                {
+                    if (!visited.Contains(adjacent))
+                    {
+                        queue.Enqueue(adjacent);
+                    }
+                }
+            }
+
+            return visited;
+        }
+    }
+}
